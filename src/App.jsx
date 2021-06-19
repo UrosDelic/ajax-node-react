df530de0003488b886c6f2fcc03a67b7737cb1e1
Uros47 <urosdelic89@gmail.com>
2021-06-19 14:32:52 +0200

    Restructure with App.jsx as main

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginForm from "./components/loginForm";
+
+const App = () => {
+  return (
+    <div>
+      <LoginForm></LoginForm>
+    </div>
+  );
+};
+
+export default App;
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -3,26 +3,34 @@ import "../assets/style.css";
 import getData from "../client/GetData";
 class LoginForm extends Component {
   state = {};
+
+  // useEffect(() => {
+  //   effect
+  //   return () => {
+  //     cleanup
+  //   };
+  // }, [input]);
+
   render() {
     return (
-      <div className='loginForm-container'>
-        <label className='label' htmlFor='email'>
+      <div className="loginForm-container">
+        <label className="label" htmlFor="email">
           Email:
         </label>
-        <input className='input' type='text' id='inputField' />
-        <label className='label' htmlFor='password'>
+        <input className="input" type="text" id="inputField" />
+        <label className="label" htmlFor="password">
           Password:
         </label>
-        <input className='input' type='text' id='passwordField' />
-        <button className='btn' id='btn-post-data'>
+        <input className="input" type="text" id="passwordField" />
+        <button className="btn" id="btn-post-data">
           Post Data
         </button>
-        <div className='displayArea'>
-          <label className='label' htmlFor='text-area'>
+        <div className="displayArea">
+          <label className="label" htmlFor="text-area">
             Show data:
           </label>
-          <textarea id='textArea' className='text-area'></textarea>
-          <button className='btn' id='btn-get-data' onClick={this.getServerData}>
+          <textarea id="textArea" className="text-area"></textarea>
+          <button className="btn" id="btn-get-data" onClick={this.getServerData}>
             Get data
           </button>
         </div>
