2b956cf49cc5b7e87dbf58a83167e026a6b69076
Uros47 <urosdelic89@gmail.com>
2021-06-20 02:00:26 +0200

    post is working

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -2,9 +2,31 @@ import React, { Component } from "react";
 import "../assets/style.css";
 import getData from "../client/GetData";
 class LoginForm extends Component {
-  state = {
-    responseData: [],
-  };
+  constructor() {
+    super();
+
+    this.state = {
+      responseData: [],
+      email: "",
+      password: "",
+    };
+
+    this.emailHandleChange = this.emailHandleChange.bind(this);
+    this.passwordHandleChange = this.passwordHandleChange.bind(this);
+    this.textareaHandleChange = this.textareaHandleChange.bind(this);
+  }
+
+  emailHandleChange(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  passwordHandleChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  textareaHandleChange(event) {
+    this.setState({ responseData: event.target.value });
+  }
 
   getServerData = () => {
     getData
@@ -17,11 +39,24 @@ class LoginForm extends Component {
       });
   };
 
+  postInputData = () => {
+    const obj = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+    getData
+      .postData(obj)
+      .then((response) => console.log(response))
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   showData = () => {
     if (this.state.responseData.length === 0) {
       return "";
     } else
-      return `email:${this.state.responseData.email} password:${this.state.responseData.password}`;
+      return `email: ${this.state.responseData.email}\npassword: ${this.state.responseData.password}`;
   };
 
   render() {
@@ -30,19 +65,37 @@ class LoginForm extends Component {
         <label className="label" htmlFor="email">
           Email:
         </label>
-        <input className="input" type="text" id="inputField" />
+        <input
+          className="input"
+          type="text"
+          id="emailField"
+          value={this.state.email}
+          onChange={this.emailHandleChange}
+        />
         <label className="label" htmlFor="password">
           Password:
         </label>
-        <input className="input" type="text" id="passwordField" />
-        <button className="btn" id="btn-post-data">
+        <input
+          className="input"
+          type="text"
+          id="passwordField"
+          value={this.state.password}
+          onChange={this.passwordHandleChange}
+        />
+        <button className="btn" id="btn-post-data" onClick={this.postInputData}>
           Post Data
         </button>
         <div className="displayArea">
           <label className="label" htmlFor="text-area">
             Show data:
           </label>
-          <textarea id="textArea" className="text-area" readOnly value={this.showData()}></textarea>
+          <textarea
+            id="textArea"
+            className="text-area"
+            readOnly
+            value={this.showData()}
+            onChange={this.textareaHandleChange}
+          ></textarea>
           <button className="btn" id="btn-get-data" onClick={this.getServerData}>
             Get data
           </button>
